811d3cba185e052d63cc8a57ff3a2672836afa1e
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-06-25 07:52:37 -0400

    makes erb template tests more specific

--- spec/lib/erb_spec.rb
+++ spec/lib/erb_spec.rb
@@ -3,15 +3,15 @@ require "spec_helper"
 describe "ERB Templates" do
   let(:abs_path) { "#{Dir.pwd}/app/views/" }
 
-  it "artists page has genres directly from associations" do
+  it "artists page lists genres directly from its associations" do
     artist_page_content = File.open("#{abs_path}/artists/show.html.erb").read
-    expect(artist_page_content).to include("artist.genres")
+    expect(artist_page_content).to include("@artist.genres")
     expect(artist_page_content).to include("genre.to_slug")
   end
 
-  it "genres page has artists directly from associations" do
+  it "genres page lists artists directly from its associations" do
     artist_page_content = File.open("#{abs_path}/genres/show.html.erb").read
-    expect(artist_page_content).to include("genre.artists")
+    expect(artist_page_content).to include("@genre.artists")
     expect(artist_page_content).to include("artist.to_slug")
   end
 end
